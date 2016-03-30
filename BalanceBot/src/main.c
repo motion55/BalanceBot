60bd712e49bd46a52434fcedd1fa4f2d618f0cec
butchtabios <butchtabios@yahoo.com>
2016-03-30 18:59:40 +0800

    end of the day snapshot

--- BalanceBot/src/main.c
+++ BalanceBot/src/main.c
@@ -35,7 +35,9 @@ float time_start;
 float time_end;
 unsigned int  response_time;
 
-
+float gain_Ap = 20.0f;
+float gain_Ai = 150.0f;
+float decay = 1.00;
 
 int main (void)
 {
@@ -72,8 +74,7 @@ int main (void)
 	#define SPEED_RATE	4
 	
 	char init_done = 0;
-	float speed_Ai_calc = 0.0;
-	float speed_Bi_calc = 0.0;
+	float speed_I_calc = 0.0;
 
 	int speed_A = 0;
 	int speed_B = 0;
@@ -84,13 +85,48 @@ int main (void)
 	uint32_t lasttime = Get_sys_count();
 	uint32_t scantime = 0;
 
-	float gain_Ap = 100.0f;
-	float gain_Ai = 15.0f;
+	float old_angle = 0;
+
 	//float gain_Bp = 40.0;
 	//float gain_Bi = 2.0;
-	float ypr_init[3];
+	float ypr_zero[3];
+	float ypr_total[3] = {0.0,0.0,0.0};
+	float ypr_zero_long[3];
+	
+#define AVERAGE_SIZE	128
+
+	while (!init_done) 
+	{
+		if (MPU6050_Loop())
+		{
+			ypr_total[0] +=  ypr[0];
+			ypr_total[1] +=  ypr[1];
+			ypr_total[2] +=  ypr[2];
+
+			loops++;
+			if (loops>=256)
+			{
+				init_done = 1;
+				ypr_zero[0] =  ypr_total[0]/loops;
+				ypr_zero[1] =  ypr_total[1]/loops;
+				ypr_zero[2] =  ypr_total[2]/loops;
+				loops = 0;
+				ypr_total[0] = 0;
+				ypr_total[1] = 0;
+				ypr_total[2] = 0;
+
+				ypr_zero_long[1] = ypr_zero[1]*AVERAGE_SIZE;
+				old_angle = ypr_zero[1];
+
+
+				#ifdef	_USE_DEBUG_CONSOLE_
+				DebugPrint("\r\n     Init %8.3f - %8.3f %8.3f %8.3f", ypr_zero[1], ypr[0], ypr[1], ypr[2]);
+				#endif
+			}
+		}
+	}
 		
-	while (true) {
+	for (;;) {
 #ifdef	_USE_DEBUG_CONSOLE_
 		DebugTask();
 #endif
@@ -99,24 +135,39 @@ int main (void)
 		if (MPU6050_Loop())
 		{
 	#if 1
-			if (init_done == 0)
-			{
-				init_done = 1;
-				ypr_init[0] =  ypr[0];
-				ypr_init[1] =  ypr[1];
-				ypr_init[2] =  ypr[2];
-			}
-			else
-			{
-				float tilt_angle = ypr[1] - ypr_init[1];
-				float speed_Ap_calc = tilt_angle * gain_Ap;
-				speed_Ai_calc += tilt_angle * gain_Ai;
+			ypr_total[0] += ypr[0];
+			ypr_total[1] += ypr[1];
+			ypr_total[2] += ypr[2];
 
-				if (speed_Ai_calc>MAX_SPEED_F) speed_Ai_calc = MAX_SPEED_F;
+			loops++;
+			if (loops>=4)
+			{
+				loops = 0;
+
+				ypr[0] = ypr_total[0]/4;
+				ypr[1] = ypr_total[1]/4;
+				ypr[2] = ypr_total[2]/4;
+
+				ypr_total[0] = 0;
+				ypr_total[1] = 0;
+				ypr_total[2] = 0;
+
+				float tilt_angle = ypr[1] - ypr_zero[1];
+				speed_I_calc += (tilt_angle-old_angle) * gain_Ai;
+				float speed_P_calc = tilt_angle * gain_Ap;
+
+//				if (tilt_angle>=0 && old_angle<0) speed_I_calc = 0;
+//				else
+//				if (tilt_angle<=0 && old_angle>0) speed_I_calc = 0;
+
+				old_angle = tilt_angle*decay;
+//				speed_I_calc *= decay;
+
+				if (speed_I_calc>MAX_SPEED_F) speed_I_calc = MAX_SPEED_F;
 				else
-				if (speed_Ai_calc<-MAX_SPEED_F) speed_Ai_calc = -MAX_SPEED_F;
-			
-				speed_A = (int)(speed_Ap_calc + speed_Ai_calc);
+				if (speed_I_calc<-MAX_SPEED_F) speed_I_calc = -MAX_SPEED_F;
+				
+				speed_A = (int)(speed_P_calc + speed_I_calc);
 				if (speed_A>MAX_SPEED) speed_A = MAX_SPEED;
 				else
 				if (speed_A<-MAX_SPEED) speed_A = -MAX_SPEED;
@@ -125,29 +176,40 @@ int main (void)
 				
 				L298_set_speed(speed_A, speed_B);
 
-				loops++;
-				if (loops>=4)
-				{
-					loops = 0;
-					#ifdef	_USE_DEBUG_CONSOLE_
-					DebugPrint("\r\n %8.3f %8i", ypr[1],speed_A);
-					#endif
-				}
+				ypr_zero_long[1] -= ypr_zero[1];
+				ypr_zero_long[1] += ypr[1];
+				ypr_zero[1] = ypr_zero_long[1]/AVERAGE_SIZE;
+
+				#ifdef	_USE_DEBUG_CONSOLE_
+				DebugPrint("\r\n %8.3f %8.3f - %8.3f %8.3f %8.3f", tilt_angle, ypr_zero[1], ypr[0], ypr[1], ypr[2]);
+				#endif
 			}
 	#else
-			uint32_t nowtime = Get_sys_count();
-			scantime += nowtime - lasttime;
-			lasttime = nowtime;
+			ypr_total[0] += ypr[0];
+			ypr_total[1] += ypr[1];
+			ypr_total[2] += ypr[2];
 
 			loops++;
-			if (loops>=16)
+			if (loops>=8)
 			{
+				ypr[0] = ypr_total[0]/8;
+				ypr[1] = ypr_total[1]/8;
+				ypr[2] = ypr_total[2]/8;
+
+				ypr_total[0] = 0;
+				ypr_total[1] = 0;
+				ypr_total[2] = 0;
+
+				uint32_t nowtime = Get_sys_count();
+				scantime = nowtime - lasttime;
+				lasttime = nowtime;
+
 				scantime /= loops;
-				#ifdef	_USE_DEBUG_CONSOLE_
-				DebugPrint("\r\n scantime = %8li ", scantime);
-				#endif
 				loops = 0;
-				scantime = 0;
+
+				#ifdef	_USE_DEBUG_CONSOLE_
+				DebugPrint("\r\n scantime = %8li - %8.3f %8.3f %8.3f", scantime, ypr[0], ypr[1], ypr[2]);
+				#endif
 			}
 	#endif
 		}
